a50a552a95041f842a63df94c71d5b8e081b0999
Fawaz Ahmed <fawazahmed0@hotmail.com>
2023-02-19 16:51:59 +0530

    add

---
+++ docusaurus.config.js
@@ -0,0 +1,180 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Quran & Hadith',
+  tagline: 'Multiple Languages & Multiple Gradings',
+  url: 'https://fawazahmed0.github.io',
+  baseUrl: '/quran-hadith-search/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/original-icon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'fawazahmed0', // Usually your GitHub org/user name.
+  projectName: 'quran-hadith-search', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/fawazahmed0/quran-hadith-search/edit/gh-pages/docsdata/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/fawazahmed0/quran-hadith-search/edit/gh-pages/docsdata/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'QuranHadiths',
+        logo: {
+          alt: 'Books',
+          src: 'img/original-icon.ico',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Books',
+          },
+          {
+            href: 'https://github.com/fawazahmed0/quran-hadith-search',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Books',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Islamic Apps',
+                href: 'https://fawazahmed0.github.io/islamic-project/',
+              },
+              {
+                label: 'Quran',
+                href: 'https://fawazahmed0.github.io/quran',
+              },
+              {
+                label: 'Hadiths',
+                href: 'https://fawazahmed0.github.io/hadiths',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/fawazahmed0/quran-hadith-search',
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+    webpack: {
+      jsLoader: (isServer) => ({
+        loader: require.resolve('esbuild-loader'),
+        options: {
+          loader: 'tsx',
+          format: isServer ? 'cjs' : undefined,
+          target: isServer ? 'node12' : 'es2017',
+        },
+      }),
+    },
+    plugins: [
+      [
+        '@docusaurus/plugin-pwa',
+        {
+          pwaHead:[
+            {
+              tagName: 'link',
+              rel: 'icon',
+              href: '/img/512x512.png',
+            },
+            {
+              tagName: 'link',
+              rel: 'manifest',
+              href: '/manifest.json', // your PWA manifest
+            },
+            {
+              tagName: 'meta',
+              name: 'theme-color',
+              content: '#007bff',
+            },
+            {
+              tagName: 'meta',
+              name: 'apple-mobile-web-app-capable',
+              content: 'yes',
+            },
+            {
+              tagName: 'meta',
+              name: 'apple-mobile-web-app-status-bar-style',
+              content: '#007bff',
+            },
+            {
+              tagName: 'link',
+              rel: 'apple-touch-icon',
+              href: '/img/512x512.png',
+            },
+            {
+              tagName: 'meta',
+              name: 'msapplication-TileImage',
+              content: '/img/512x512.png',
+            },
+            {
+              tagName: 'meta',
+              name: 'msapplication-TileColor',
+              content: '#007bff',
+            },
+          ]
+        }
+      ],
+    ],
+};
+
+module.exports = config;
---
+++ genscript.js
@@ -0,0 +1,242 @@
+const fs = require('fs-extra')
+const path = require('path')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+let hadithLinks = ["https://cdn.jsdelivr.net/gh/fawazahmed0/hadith-api@1/", "https://raw.githubusercontent.com/fawazahmed0/hadith-api/1/"]
+let quranLinks = ["https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/", "https://raw.githubusercontent.com/fawazahmed0/quran-api/1/"]
+let extensions = [".min.json", ".json"]
+let bigJSON = {}
+
+const chaptersJSON = { 'Al-Faatiha': 'The Opening', 'Al-Baqara': 'The Cow', 'Aal-i-Imraan': 'The Family of Imraan', 'An-Nisaa': 'The Women', 'Al-Maaida': 'The Table', "Al-An'aam": 'The Cattle', "Al-A'raaf": 'The Heights', 'Al-Anfaal': 'The Spoils of War', 'At-Tawba': 'The Repentance', Yunus: 'Jonas', Hud: 'Hud', Yusuf: 'Joseph', "Ar-Ra'd": 'The Thunder', Ibrahim: 'Abraham', 'Al-Hijr': 'The Rock', 'An-Nahl': 'The Bee', 'Al-Israa': 'The Night Journey', 'Al-Kahf': 'The Cave', Maryam: 'Mary', 'Taa-Haa': 'Taa-Haa', 'Al-Anbiyaa': 'The Prophets', 'Al-Hajj': 'The Pilgrimage', 'Al-Muminoon': 'The Believers', 'An-Noor': 'The Light', 'Al-Furqaan': 'The Criterion', "Ash-Shu'araa": 'The Poets', 'An-Naml': 'The Ant', 'Al-Qasas': 'The Stories', 'Al-Ankaboot': 'The Spider', 'Ar-Room': 'The Romans', Luqman: 'Luqman', 'As-Sajda': 'The Prostration', 'Al-Ahzaab': 'The Clans', Saba: 'Sheba', Faatir: 'The Originator', Yaseen: 'Yaseen', 'As-Saaffaat': 'Those drawn up in Ranks', Saad: 'The letter Saad', 'Az-Zumar': 'The Groups', Ghafir: 'The Forgiver', Fussilat: 'Explained in detail', 'Ash-Shura': 'Consultation', 'Az-Zukhruf': 'Ornaments of gold', 'Ad-Dukhaan': 'The Smoke', 'Al-Jaathiya': 'Crouching', 'Al-Ahqaf': 'The Dunes', Muhammad: 'Muhammad', 'Al-Fath': 'The Victory', 'Al-Hujuraat': 'The Inner Apartments', Qaaf: 'The letter Qaaf', 'Adh-Dhaariyat': 'The Winnowing Winds', 'At-Tur': 'The Mount', 'An-Najm': 'The Star', 'Al-Qamar': 'The Moon', 'Ar-Rahmaan': 'The Beneficent', 'Al-Waaqia': 'The Inevitable', 'Al-Hadid': 'The Iron', 'Al-Mujaadila': 'The Pleading Woman', 'Al-Hashr': 'The Exile', 'Al-Mumtahana': 'She that is to be examined', 'As-Saff': 'The Ranks', "Al-Jumu'a": 'Friday', 'Al-Munaafiqoon': 'The Hypocrites', 'At-Taghaabun': 'Mutual Disillusion', 'At-Talaaq': 'Divorce', 'At-Tahrim': 'The Prohibition', 'Al-Mulk': 'The Sovereignty', 'Al-Qalam': 'The Pen', 'Al-Haaqqa': 'The Reality', "Al-Ma'aarij": 'The Ascending Stairways', Nooh: 'Noah', 'Al-Jinn': 'The Jinn', 'Al-Muzzammil': 'The Enshrouded One', 'Al-Muddaththir': 'The Cloaked One', 'Al-Qiyaama': 'The Resurrection', 'Al-Insaan': 'Man', 'Al-Mursalaat': 'The Emissaries', 'An-Naba': 'The Announcement', "An-Naazi'aat": 'Those who drag forth', Abasa: 'He frowned', 'At-Takwir': 'The Overthrowing', 'Al-Infitaar': 'The Cleaving', 'Al-Mutaffifin': 'Defrauding', 'Al-Inshiqaaq': 'The Splitting Open', 'Al-Burooj': 'The Constellations', 'At-Taariq': 'The Morning Star', "Al-A'laa": 'The Most High', 'Al-Ghaashiya': 'The Overwhelming', 'Al-Fajr': 'The Dawn', 'Al-Balad': 'The City', 'Ash-Shams': 'The Sun', 'Al-Lail': 'The Night', 'Ad-Dhuhaa': 'The Morning Hours', 'Ash-Sharh': 'The Consolation', 'At-Tin': 'The Fig', 'Al-Alaq': 'The Clot', 'Al-Qadr': 'The Power, Fate', 'Al-Bayyina': 'The Evidence', 'Az-Zalzala': 'The Earthquake', 'Al-Aadiyaat': 'The Chargers', "Al-Qaari'a": 'The Calamity', 'At-Takaathur': 'Competition', 'Al-Asr': 'The Declining Day, Epoch', 'Al-Humaza': 'The Traducer', 'Al-Fil': 'The Elephant', Quraish: 'Quraysh', "Al-Maa'un": 'Almsgiving', 'Al-Kawthar': 'Abundance', 'Al-Kaafiroon': 'The Disbelievers', 'An-Nasr': 'Divine Support', 'Al-Masad': 'The Palm Fibre', 'Al-Ikhlaas': 'Sincerity', 'Al-Falaq': 'The Dawn', 'An-Naas': 'Mankind' };
+const arabicChapters = Object.keys(chaptersJSON).map(e=>e.replace("'",""))
+const englishChapters = Object.values(chaptersJSON)
+
+async function test() {
+    let docsDir = process.env.TEST_BUILD ? 'testdocs' : 'docs'
+    let editionsJSON = await getJSON('editions')
+    let quranEditionsJSON = await getJSON('editions', quranLinks)
+    let isocodes = await getJSON('isocodes/iso-codes', quranLinks)
+
+    let hadithPath = path.join(__dirname, docsDir,'Hadiths')
+    let quranPath = path.join(__dirname, docsDir,'Quran')
+
+    for (let [bareedition, value] of Object.entries(editionsJSON)) {
+        let sortedCollections = editionsJSON[bareedition].collection.sort((a,b)=>a.language.localeCompare(b.language))
+        for (let collection of sortedCollections) {
+
+            let edition = collection.name;
+            let lang = collection.language;
+            let dirval = collection.direction;
+
+
+            let data = await getJSON(`editions/${edition}`)
+            let languageHeading = `## ${lang}`
+
+            let hadiths = data.hadiths
+
+            for (let hadith of hadiths) {
+                let pathToSave = path.join(hadithPath,editionsJSON[bareedition].name,`${editionsJSON[bareedition].name}  ${Math.floor(hadith.hadithnumber)}.md`)
+                let dataToSave = getHadithCardElem(hadith, dirval, lang, isocodes)
+
+                // Add table of content
+                if(pathToSave in bigJSON === false)
+                addToBigJSON(pathToSave, [...new Set(sortedCollections.map(e=> `[${e.language}](#${e.language.toLowerCase().replaceAll(' ','-')})`))].join('\n\n') )
+                
+                
+                // save language if doesn't exists
+                if(!bigJSON[pathToSave].includes(languageHeading))
+                addToBigJSON(pathToSave, languageHeading )
+
+                addToBigJSON(pathToSave, dataToSave )
+            }
+            
+
+        }
+        
+    }
+    let sortedEditionValues = Object.values(quranEditionsJSON).sort((a,b)=>a.language.localeCompare(b.language))
+    for (let value of sortedEditionValues) {
+        let data = await getJSON(`editions/${value.name}`, quranLinks)
+        let languageHeading = `## ${value.language}`
+
+        for(let quran of data.quran){
+            let chapterName = `Chapter ${quran.chapter} ${arabicChapters[quran.chapter - 1].replaceAll('-',' ')}`
+            let pathToSave = path.join(quranPath,chapterName,`${chapterName} Verse  ${quran.verse}.md`)
+            let dataToSave =  getQuranCardElem(quran, value.direction, value.language,value.author, isocodes) 
+            
+            // Add table of content
+            if(pathToSave in bigJSON === false)
+            addToBigJSON(pathToSave, [...new Set(sortedEditionValues.map(e=> `[${e.language}](#${e.language.toLowerCase().replaceAll(' ','-')})`))].join('\n\n') )
+
+            // save language if doesn't exists
+                            if(!bigJSON[pathToSave].includes(languageHeading))
+                            addToBigJSON(pathToSave, languageHeading )
+            
+                            addToBigJSON(pathToSave, dataToSave )
+                            
+        }
+
+
+
+
+    }
+    // only generate n random files if it's test builds
+    if(process.env.TEST_BUILD){
+       // number of files to generate if it's test build
+       let maxFiles = 100
+       let len = Object.keys(bigJSON).length
+       let randomArr = Array.from(Array(maxFiles).keys()).map(e=> getRandomArbitrary(len))
+       let filteredEntries = Object.entries(bigJSON).filter((e,i)=>randomArr.includes(i))
+       bigJSON = Object.fromEntries(filteredEntries)
+    }
+
+    for(let [pathToSave, dataArr] of Object.entries(bigJSON)){
+        let data = dataArr.join('\n\n')
+        // max file size for code search to work in github
+        // https://docs.github.com/en/search-github/searching-on-github/searching-code#:~:text=only%20files%20smaller%20than%20384%20kb%20are%20searchable.
+        let ghFileLen = 349 * 1000  // 349 kb
+        fs.outputFileSync(pathToSave, data)
+        // Save in wiki, if not searchable in github code search
+        if(Buffer.byteLength(data, 'utf8') > ghFileLen - 2000)
+        fs.outputFileSync(path.join(__dirname, 'wiki',path.basename(pathToSave)), data)
+    }
+
+}
+
+test()
+
+
+// pass hadith object & get card element with all hadith info in it
+function getHadithCardElem(hadith, dirval, lang, isocodes) {
+    let str = ''
+    let lowerLang = lang.toLowerCase()
+    let mainText = `\n<div dir="${dirval}" lang="${isocodes[lowerLang].iso1 ? isocodes[lowerLang].iso1 : isocodes[lowerLang].iso2}" style={{fontSize:'larger',backgroundColor:'#f8f9fa',padding:20}}>
+${cleanText(hadith.text)}
+</div>\n`;
+    str+=`<div style={{backgroundColor:'#f8f9fa',padding:20, marginBottom: 10}}>`
+    if (hadith.grades.length > 0) {
+        str += `<table>
+      <thead>
+        <tr>
+          <th>Grade</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+`
+    }
+
+    for (let grade of hadith.grades)
+        str += `<tr><td>${capitalize(grade.grade)}</td><td>${grade.name}</td></tr>`
+
+    if (hadith.grades.length > 0)
+        str += `</tbody></table>`
+
+
+    if ("hadithnumber" in hadith) {
+        str += `<table>
+        <thead>
+          <tr>
+            <th>References:</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>`
+        str += `<tr><td>Hadith No</td><td>${hadith.hadithnumber}</td></tr>`
+    }
+    if ("arabicnumber" in hadith) {
+        str += `<tr><td>Arabic No</td><td>${hadith.arabicnumber}</td></tr>`
+    }
+
+    if ("reference" in hadith) {
+        str += `<tr><td>Reference</td><td>${Object.entries(hadith.reference).flat().map(e => capitalize(e)).join(' ')}</td></tr>`
+    }
+    str += `</tbody></table></div>`
+
+    return mainText + str.replace(/\s+/gi, ' ')
+}
+
+
+// pass hadith object & get card element with all hadith info in it
+function getQuranCardElem(quran, dirval, lang,authorName, isocodes) {
+    let str = ''
+    let lowerLang = lang.toLowerCase()
+    let mainText = `\n<div dir="${dirval}" lang="${isocodes[lowerLang].iso1 ? isocodes[lowerLang].iso1 : isocodes[lowerLang].iso2}" style={{fontSize:'larger',backgroundColor:'#f8f9fa',padding:20}}>
+${cleanText(quran.text)}
+</div>\n`;
+    str+=`<div style={{backgroundColor:'#f8f9fa',padding:20, marginBottom: 10}}>`
+
+        str += `<table>
+      <thead>
+        <tr>
+          <th>Details</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+`
+    
+
+        str += `<tr><td>Author</td><td>${authorName}</td></tr>`
+
+        str += `</tbody></table></div>`
+
+    return mainText + str.replace(/\s+/gi, ' ')
+}
+
+
+function capitalize(words) {
+    return words.toString().toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g, match => match.toUpperCase()).trim()
+}
+
+function addToBigJSON(key, value) {
+
+    if(key in bigJSON)
+        bigJSON[key].push(value)
+    else
+        bigJSON[key] = [value]
+
+}
+
+// https://www.shawntabrizi.com/code/programmatically-fetch-multiple-apis-parallel-using-async-await-javascript/
+// Get links async i.e in parallel
+async function getJSON(endpoints, links) {
+    let returnSingle = false
+    if (!Array.isArray(endpoints)) {
+        endpoints = [endpoints]
+        returnSingle = true
+    }
+    let result = await Promise.all(
+        endpoints.map(endpoint => fetchWithFallback(getURLs(endpoint, links)).then(response => response.json()))
+    ).catch(console.error)
+    if (returnSingle)
+        return result[0]
+    return result
+}
+
+
+async function fetchWithFallback(links, obj) {
+    let response;
+    for (let link of links) {
+        try {
+            response = await fetch(link, obj)
+            if (response.ok)
+                return response
+        } catch (e) { }
+    }
+    return response
+}
+
+// convert endpoint into multiple urls, including fallback urls
+function getURLs(endpoint, links) {
+    links = links || hadithLinks
+    return extensions.map(ext => links.map(e => e + endpoint + ext)).flat()
+}
+
+function getRandomArbitrary(max) {
+    return Math.floor(Math.random() * max)
+}
+
+function cleanText(str){
+    return str.replace(/`/gi,"'").replace(/\{/gi,"(").replace(/\}/gi,")").replace(/\</gi,"(").replace(/\>/gi,")")
+}
